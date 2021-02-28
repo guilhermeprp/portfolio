2fbd67ae4a59702430fe8471e566309dba4a52e9
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2021-02-28 02:57:52 -0300

    redesign da home page

--- next_app/src/assets/styles/homeStyles.js
+++ next_app/src/assets/styles/homeStyles.js
@@ -1,69 +1,68 @@
 import styled from "styled-components";
 
 export const HomePage = styled.div`
-  .home-background {
-    height: 120vh;
-    opacity: 0.1;
-    background-repeat: no-repeat;
-    background-size: cover;
-    z-index: -1;
-    margin-top: -50px;
-    animation: bgAnimation 3s normal;
-  }
-  .home-bg-gradient {
+  .home-body {
     height: 100vh;
-    width: 100%;
-    z-index: 0;
-    opacity: 1;
-    background-image: linear-gradient(transparent 60%, #202125 90%);
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-
-  .row {
-    right: 0;
-    top: 50vh;
-    z-index: 1;
-    position: absolute;
     width: 100vw;
-    text-align-last: right;
-    .wrapper {
-      padding: 0px 10vw;
+    overflow: hidden;
+    video {
+      z-index: -1;
+    }
 
-      .box-titles {
-        display: inline-block;
-        width: 70%;
-        height: 100%;
+    .home-bg-gradient {
+      height: 100vh;
+      width: 100%;
+      z-index: 0;
+      opacity: 1;
+      background-image: linear-gradient(transparent 60%, #010101 90%);
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
 
-        .title1 {
-          // color: #c0c0c2;
-          color: ${(props) => props.theme.colors.thirdtitle};
-          font-size: 20px;
-          font-weight: 100;
-          letter-spacing: 10px;
+    .row {
+      right: 0;
+      top: 50vh;
+      z-index: 1;
+      position: absolute;
+      width: 100vw;
+      text-align-last: right;
+      .wrapper {
+        padding: 0px 10vw;
 
-          margin: 0;
-        }
+        .box-titles {
+          display: inline-block;
+          width: 70%;
+          height: 100%;
 
-        .title2 {
-          display: block;
-          color: white;
-          font-size: 100px;
-          letter-spacing: 5px;
-          padding-bottom: 5px;
+          .title1 {
+            color: ${(props) => props.theme.colors.title};
+            font-size: 20px;
+            font-weight: 100;
+            letter-spacing: 10px;
 
-          margin: 0;
-          text-align-last: right;
-          float: right;
-          text-align: -webkit-right;
-        }
+            margin: 0;
+          }
 
-        .title3 {
-          float: right;
-          text-align: end;
-          color: ${(props) => props.theme.colors.subtitle};
-          font-size: 30px;
+          .title2 {
+            display: block;
+            color: white;
+            font-size: 100px;
+            letter-spacing: 5px;
+            padding-bottom: 5px;
+
+            margin: 0;
+            text-align-last: right;
+            float: right;
+            text-align: right;
+          }
+
+          .title3 {
+            float: right;
+            text-align: end;
+            color: ${(props) => props.theme.colors.subtitle};
+            font-size: 30px;
+          }
         }
       }
     }
@@ -81,7 +80,7 @@ export const HomePage = styled.div`
   }
 
   @media screen and (max-width: 768px) and (min-width: 577px) {
-    .home-background {
+    .home-body {
       height: 110vh;
     }
     .row {
@@ -108,39 +107,49 @@ export const HomePage = styled.div`
   }
 
   @media screen and (max-width: 576px) {
-    .row {
-      width: 90vw;
-      left: 20px;
-      text-align-last: left;
-      float: left;
-      top: 60vh;
-      .wrapper {
-        padding: 0px 10px;
-        .box-titles {
-          .title1 {
-            font-size: 11px;
-            text-align-last: left;
-            float: left;
-            padding-left: 5px;
-            width: max-content;
-            color: #696d79;
-          }
-          .title2 {
-            font-size: 50px;
-            text-align-last: left;
-            float: left;
-            line-height: 60px;
-            margin-top: 5px;
-            text-align: -webkit-left;
-          }
-          .title3 {
-            font-size: 16px;
-            text-align-last: left;
-            float: left;
-            padding-left: 5px;
-            max-width: 100vw;
-            width: 80vw;
-            text-align: start;
+    .home-body {
+      .background-video div {
+        width: 100vh !important;
+        height: 215vw !important;
+        left: -50%;
+        position: fixed;
+        transform: rotate(-90deg);
+        z-index: 0;
+      }
+      .row {
+        z-index: 1;
+        width: 90vw !important;
+        left: 20px !important;
+        text-align-last: left !important;
+        float: left !important;
+        top: 60vh !important;
+        .wrapper {
+          padding: 0px 10px !important;
+          .box-titles {
+            .title1 {
+              font-size: 11px !important;
+              text-align-last: left !important;
+              float: left !important;
+              padding-left: 5px !important;
+              width: max-content !important;
+            }
+            .title2 {
+              font-size: 50px !important;
+              text-align-last: left !important;
+              float: left !important;
+              line-height: 60px !important;
+              margin-top: 5px !important;
+              text-align: -webkit-left !important;
+            }
+            .title3 {
+              font-size: 16px !important;
+              text-align-last: left !important;
+              float: left !important;
+              padding-left: 5px !important;
+              max-width: 100vw !important;
+              width: 80vw !important;
+              text-align: start !important;
+            }
           }
         }
       }
--- next_app/src/components/home.jsx
+++ next_app/src/components/home.jsx
@@ -1,24 +1,38 @@
 import React from "react";
+import ReactPlayer from "react-player";
 import { HomePage } from "../assets/styles/homeStyles.js";
-import homeBackground from "../assets/images/background-img.png";
+// import homeBackground from "../assets/images/background-img.png";
+// import backgroundVideo from "../assets/video/background-video.mp4";
 
 console.log(process.env.NODE_ENV);
 
 function Home() {
   return (
     <HomePage>
-      <img className="home-background" src={homeBackground} />
-      <div className="home-bg-gradient"></div>
-      <div className="row">
-        <div className="wrapper">
-          <div className="box-titles">
-            <h5 className="title1">GUILHERME PIMENTA</h5>
-            <h1 className="title2">Front End Developer</h1>
-            <h3 className="title3">
-              Together we will build modern interfaces for apps and websites.
-              <br />
-              Hands on and let's start to work.
-            </h3>
+      <div className="home-body">
+        <ReactPlayer
+          className="background-video"
+          loop="true"
+          muted="true"
+          width="1920px"
+          height="1080px"
+          playing="true"
+          controls="false"
+          url="https://www.youtube.com/watch?v=ae6_Nq4AWDA&t"
+        ></ReactPlayer>
+
+        <div className="home-bg-gradient"></div>
+        <div className="row">
+          <div className="wrapper">
+            <div className="box-titles">
+              <h5 className="title1">GUILHERME PIMENTA</h5>
+              <h1 className="title2">Front End Developer</h1>
+              <h3 className="title3">
+                Together we will build modern interfaces for apps and websites.
+                <br />
+                Hands on and let's start to work.
+              </h3>
+            </div>
           </div>
         </div>
       </div>
